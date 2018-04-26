e87c90ac6f1f66fcaac4a288bec51c5987a79754
DESKTOP-MA2BPC6\Herbert <herbertfilhik@gmail.com>
2018-04-26 11:09:15 -0300

    teste

--- executetest.sh
+++ executetest.sh
@@ -1,11 +1,2 @@
-:'cd /var/lib/jenkins/workspace/pocbrowserstack
-gem install bundler
-
-cd /var/lib/jenkins/workspace/pocbrowserstack
-bundle install
-
-cd /var/lib/jenkins/workspace/pocbrowserstack
-cucumber -t @validar_carrinho -f json -o relatorios/cucumber-json-report.json'
-
 cd /opt/slackmessenger/DuSlackMessage
 python3 messenger.py -c "testeapi" -m "Teste - Slack"
